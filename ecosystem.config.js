7dd8d946eb11327d00c463941bc651512bf9a38d
Andrew Carter <andrew@podium-network.com>
2019-12-16 21:09:28 +0000

    Adding PM2 config.

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+	apps: [{
+		name: 'podium-admin-server',
+		script: 'npm',
+		args: 'start-production'
+	}],
+
+	deploy: {
+		production: {
+
+			user: 'ubuntu',
+			ref: 'origin/master',
+			repo: 'https://github.com/carter-andrewj/podium-admin-server.git',
+			path: '/home/ubuntu/podium-admin-server',
+			'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+
+			// CHANGE THESE ON REDEPLOYMENT TO NEW SERVER
+			host: 'ec2-35-178-193-209.eu-west-2.compute.amazonaws.com',
+			key: '~/.ssh/podium-admin-server.pem',
+
+		}
+	}
+	
+}
